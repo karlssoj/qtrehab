6d6d904af1170ab928470fb36cab8016c811c0e7
Jonny Karlsson <karlssoj@arcada.fi>
2026-05-06 17:14:38 +0300

    Added shoulder ROM analysis

---
+++ axelns_rorelseomfang/exercise_config.py
@@ -0,0 +1,7 @@
+CONFIG = {
+    "name": 'Axelns rorelseomfang',
+    "camera_view": 'side',
+    "client_instructions": 'Ställ dig med ena sidan mot kameran. Utför sedan lugna och kontrollerade armrörelser upp och ned. Håll armen rak under hela rörelsen. Vid flexion, lyft armen framåt och uppåt så långt du kan. Vid extension, för armen bakåt så långt det känns bekvämt.',
+    "session_duration_secs": 10,
+    "feedback_mode": ["after_rep", "after_exercise"],
+}
